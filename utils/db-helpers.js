3eb8cf897b2569a3063debb271604a854ce090a5
robert-brown96 <robert.brown@saascg.com>
2022-05-01 19:18:33 -0500

    add update

--- utils/db-helpers.js
+++ utils/db-helpers.js
@@ -49,6 +49,13 @@ const getDbItem = async (tableName, db, { partitionKey, value }) => {
     }
 };
 
+/**
+ *
+ *
+ * @param {String} tableName
+ * @param {DocumentClient} db
+ * @return {*}
+ */
 const scanTable = async (tableName, db) => {
     try {
         const listParams = {
@@ -70,4 +77,62 @@ const scanTable = async (tableName, db) => {
     }
 };
 
-module.exports = { getDbItem, scanTable };
+/**
+ *
+ *
+ * @param {String} tableName
+ * @param {DocumentClient} db
+ * @param {Object} keyValue
+ * @param {Object} updateValues
+ */
+const updateDbItem = async (tableName, db, keyValue, data) => {
+    try {
+        const updateKeys = Object.keys(data);
+        let setVals = updateKeys.reduce((result, k) => {
+            console.log(`checking key ${k}`);
+            if (data[k] && data[k] !== "" && data[k] !== "null")
+                result.push(`${k} = :${k}`);
+            return result;
+        }, []);
+        console.log(`sets ${JSON.stringify(setVals)}`);
+        let attributeValues = {};
+        setVals.forEach(element => {
+            attributeValues[`:${element}`] = data[element];
+        });
+        console.log(`attribute vals ${JSON.stringify(attributeValues)}`);
+
+        const updateParams = {
+            TableName: tableName,
+            Key: keyValue,
+            UpdateExpression: `SET ${setVals.join(",")}`,
+            ExpressionAttributeValues: attributeValues,
+            ReturnValues: "ALL_NEW"
+        };
+
+        return await db
+            .update(updateParams)
+            .promise()
+            .then(res => {
+                return {
+                    statusCode: 200,
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(res)
+                };
+            })
+            .catch(e => {
+                return {
+                    statusCode: 404,
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(e)
+                };
+            });
+    } catch (e) {
+        console.error(`UNCAUGHT ERROR ${e}`);
+        return {
+            statusCode: 402,
+            body: JSON.stringify(e)
+        };
+    }
+};
+
+module.exports = { getDbItem, scanTable, updateDbItem };
