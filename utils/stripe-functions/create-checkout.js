a6ac9905b0797f506363d8c9e44d3f9b7cba86ee
robert-brown96 <robert.brown@saascg.com>
2022-05-02 18:05:06 -0500

    fix stripe auth

--- utils/stripe-functions/create-checkout.js
+++ utils/stripe-functions/create-checkout.js
@@ -1,28 +1,24 @@
-const stripe = require("stripe");
+const Stripe = require("stripe");
 
 const createCheckout = async options => {
     try {
         const { secretKey, itemDetails } = options;
+        const stripe = Stripe(secretKey);
 
-        const stripeCheckoutObj = await stripe.checkout.sessions.create(
-            {
-                success_url: "https://google.com",
-                cancel_url: "https://yahoo.com",
-                line_items: [
-                    {
-                        price_data: {
-                            currency: "usd",
-                            product_data: { name: `stripe checkout` },
-                            unit_amount: 1000
-                        }
+        const stripeCheckoutObj = await stripe.checkout.sessions.create({
+            success_url: "https://google.com",
+            cancel_url: "https://yahoo.com",
+            line_items: [
+                {
+                    price_data: {
+                        currency: "usd",
+                        product_data: { name: `stripe checkout` },
+                        unit_amount: 1000
                     }
-                ],
-                mode: "payment"
-            },
-            {
-                apiKey: secretKey
-            }
-        );
+                }
+            ],
+            mode: "payment"
+        });
 
         return stripeCheckoutObj;
     } catch (e) {
